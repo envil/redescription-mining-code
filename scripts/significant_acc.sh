845002c54ace569325122d145742e0ca9bc0463b
Esther Galbrun <egalbrun@scm.gforge.inria.fr>
2010-04-23 09:13:13 +0000

    Counts the number of copies
    git-svn-id: file:///home/galbrun/TKTL/redescriptors/old_svn/redescriptions-svn/sandbox@42 8b04a84e-e702-47ad-8570-c905424f7f45

--- scripts/significant_acc.sh
+++ scripts/significant_acc.sh
@@ -7,11 +7,12 @@ RATIO=0.01
 OUT=${RES_PREF}'_'${TYPE}'0.sign'$RULE_EXT
 
 awk -F'\t' '{ print $3 }' ${RES_MATCH} | sort -n -r > summup.tmp
+NB_FILES=$(echo ${RES_MATCH} | wc -w )
 NB_ACCS=$(wc -l summup.tmp | cut -f 1 -d ' ' )
 LINE_NB=$(echo $NB_ACCS*$RATIO | bc | cut -f 1 -d '.' )
 SIGN_ACC=$(head -n $LINE_NB summup.tmp | tail -1)
 rm summup.tmp
 
-echo -e "Significant results: acc>= $SIGN_ACC line $NB_ACCS x $RATIO = $LINE_NB" > $OUT
+echo -e "Significant results: acc>= $SIGN_ACC line $NB_ACCS x $RATIO = $LINE_NB in $NB_FILES copies" > $OUT
 AWK_SCRIPT='{ if ($3>='${SIGN_ACC}') print $0 "\t" 1; else print $0 "\t" 1 }'
 awk -F'\t' "${AWK_SCRIPT}" $ORG_RES >> $OUT
