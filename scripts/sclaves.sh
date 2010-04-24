ef17bf9114fc4a42fe426e065a5bc03f76968302
Esther Galbrun <egalbrun@scm.gforge.inria.fr>
2010-04-24 09:05:13 +0000

    forgotten modification
    git-svn-id: file:///home/galbrun/TKTL/redescriptors/old_svn/redescriptions-svn/sandbox@47 8b04a84e-e702-47ad-8570-c905424f7f45

--- scripts/sclaves.sh
+++ scripts/sclaves.sh
@@ -2,39 +2,22 @@ FIRST_PROCESS=${1}
 LAST_PROCESS=${2}
 TODO_FILE=${3}
 MASTER_ID=${4}
-
-function f_run_sub {
-
-      local PROC_ID=${1}
-      local TODO_F=${2}
-      echo "Starting" $PROC_ID $TODO_F
-       while [ 1 ] 
-       do
-	   lockfile-create $TODO_F
-	   COMM_ACTION=$(grep -m 1 '^0' $TODO_F | cut -f 2- | sed "s/__PROC_ID__/${PROC_ID}/g")
-	   sed -i '0,/^0\t/s//1\t/1' $TODO_F
-	   lockfile-remove $TODO_F
-	   if (( ${#COMM_ACTION} > 0 )); then
-	       echo "$PROC_ID executing $COMM_ACTION ..."
-	       $COMM_ACTION	  
-	   else
-	       sleep 180
-	   fi
-      done
-      echo "$PROC_ID died..."
-}
+echo "Sclaves $HOSTNAME ${MASTER_ID}_$FIRST_PROCESS ${MASTER_ID}_$LAST_PROCESS $TODO_FILE"
+echo "Sclaves $HOSTNAME ${MASTER_ID}_$FIRST_PROCESS ${MASTER_ID}_$LAST_PROCESS $TODO_FILE" >> $TODO_FILE.logmasters
 
 
 for (( i=${FIRST_PROCESS}; i<=${LAST_PROCESS}; i++ ))
 do
-    (f_run_sub $MASTER_ID$i $TODO_FILE)& 
+    echo "$HOSTNAME starting ${MASTER_ID}_$i $TODO_FILE"
+    echo "$HOSTNAME starting ${MASTER_ID}_$i $TODO_FILE" >> $TODO_FILE.logmasters
+    (~/redescriptors/sandbox/scripts/run_subprocess.sh ${MASTER_ID}_$i $TODO_FILE)& 
     sleep 1
 done
 
-echo "Sclaves $HOSTNAME $MASTER_ID$FIRST_PROCESS $MASTER_ID$LAST_PROCESS" >> $TODO_FILE.sclaves
+echo "Sclaves $HOSTNAME ${MASTER_ID}_$FIRST_PROCESS ${MASTER_ID}_$LAST_PROCESS" >> $TODO_FILE.sclaves
 jobs -l >> $TODO_FILE.sclaves
 
 echo "===================================================================="
-echo "Sclaves $HOSTNAME $MASTER_ID$FIRST_PROCESS $MASTER_ID$LAST_PROCESS"
+echo "Sclaves $HOSTNAME ${MASTER_ID}_$FIRST_PROCESS ${MASTER_ID}_$LAST_PROCESS"
 jobs -l
 echo "===================================================================="
