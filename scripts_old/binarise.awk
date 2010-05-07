af66153245513d5023c2347e14c9390b7bc0da11
Esther Galbrun <egalbrun@scm.gforge.inria.fr>
2010-05-07 11:23:11 +0000

    moved old scripts to Tags This line, and those below, will be ignored--
    M    trunk/greedyRedescriptions/README
    D    sandbox/scripts_old
    A    tags/booleanMiner_0.3
    A    tags/booleanMiner_0.3/scripts
    AM   tags/booleanMiner_0.3/scripts/map_rajapaja.sh
    AM   tags/booleanMiner_0.3/scripts/kill_sclaves.sh
    AM   tags/booleanMiner_0.3/scripts/name_the_raja.sh
    A    tags/booleanMiner_0.3/scripts/classData.py
    AM   tags/booleanMiner_0.3/scripts/preprocess_rajapaja.sh
    AM   tags/booleanMiner_0.3/scripts/name_dblp.sh
    A    tags/booleanMiner_0.3/scripts/load_matrix.m
    AM   tags/booleanMiner_0.3/scripts/funnel.sh
    A    tags/booleanMiner_0.3/scripts/classRedescriptionDraft.py
    AM   tags/booleanMiner_0.3/scripts/sclaves.sh
    AM   tags/booleanMiner_0.3/scripts/greedyRuleFinder6.py
    AM   tags/booleanMiner_0.3/scripts/significant_acc.sh
    AM   tags/booleanMiner_0.3/scripts/utilsIO.py
    AM   tags/booleanMiner_0.3/scripts/sanityChecker.py
    A    tags/booleanMiner_0.3/scripts/classRule.py
    AM   tags/booleanMiner_0.3/scripts/run_subprocess.sh
    AM   tags/booleanMiner_0.3/scripts/display_graph_rules.py
    A    tags/booleanMiner_0.3/scripts/classRedescription.py
    A    tags/booleanMiner_0.3/scripts/display_map.m
    AM   tags/booleanMiner_0.3/scripts/preprocess_dblp.sh
    A    tags/booleanMiner_0.3/scripts/demap_indices.awk
    A    tags/booleanMiner_0.3/scripts/utilsStats.py
    A    tags/booleanMiner_0.3/scripts/filter_data.m
    A    tags/booleanMiner_0.3/scripts/classSouvenirs.py
    AM   tags/booleanMiner_0.3/scripts/generate_randomize.sh
    AM   tags/booleanMiner_0.3/scripts/postProcess.py
    A    tags/booleanMiner_0.3/scripts/binarise.awk
    AM   tags/booleanMiner_0.3/scripts/run.sh
    A    tags/booleanMiner_0.3/scripts/save_matrix.m
    A    tags/booleanMiner_0.3/scripts/extract_names.awk
    A    tags/booleanMiner_0.3/scripts/classBestsDraft.py
    
    
    git-svn-id: file:///home/galbrun/TKTL/redescriptors/old_svn/redescriptions-svn/sandbox@93 8b04a84e-e702-47ad-8570-c905424f7f45

--- scripts_old/binarise.awk
+++
@@ -1,7 +0,0 @@
-## Usage awk -f <this file> <dat_data_with_cardinalities> > <binary_dat_data>
-BEGIN {
-    FS="\t"
-}
-{ 
-    print $1 "\t" $2 "\t" ($3 != 0)
-}
--- scripts_old/kill_sclaves.sh
+++
@@ -1,9 +0,0 @@
-TODO_FILE=${1}
-
-echo "Killed so far" >> $TODO_FILE.sclaves
-COUNT_SCL=$(grep -c 'Running' $TODO_FILE.sclaves)
-
-for (( i=1; i<=${COUNT_SCL}; i++ ))
-do
-    echo -e "0\texit" >> $TODO_FILE
-done
