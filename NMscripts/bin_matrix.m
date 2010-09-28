68e5f6c6f63e3f40be5319d51b0793c353e257c3
Esther Galbrun <egalbrun@scm.gforge.inria.fr>
2010-09-28 08:13:57 +0000

    binning matrices
    git-svn-id: file:///home/galbrun/TKTL/redescriptors/old_svn/redescriptions-svn/sandbox@121 8b04a84e-e702-47ad-8570-c905424f7f45

---
+++ NMscripts/bin_matrix.m
@@ -0,0 +1,49 @@
+function [mat_bin, bounds] = bin_matrix(mat_real, N, how)
+
+if nargin < 3, how = 'means'; end
+if nargin < 2, N = 10; end
+
+[n,m] = size(mat_real);
+if (length(N) ~= m)
+    N = N(1)*ones(1,m);
+end
+eps = 0.001;
+mat_bin = sparse(n,sum(N));
+bounds = cell(1,m);
+
+if strcmp(how, 'width')
+    for i=1:m
+        mi = min(mat_real(:,i));
+        ma = max(mat_real(:,i))+eps;
+        width = (ma-mi)/N(i);
+        bounds{i} = mi + (1:N(i)-1)'*width;
+        for j=1:N(i)
+            mat_bin(((mat_real(:,i)>=(mi+(j-1)*width)) & (mat_real(:,i)<(mi+(j)*width))),sum(N(1:i-1))+j) = 1;
+        end
+    end
+
+elseif strcmp(how, 'height')
+    for i=1:m
+        ptiles = prctile(mat_real(:,i), [0:N(i)]*100/N(i));
+        ptiles(end) = ptiles(end)+eps;
+        bounds{i} = ptiles(2:end-1)';
+        for j=1:N(i)
+            mat_bin(((mat_real(:,i)>=ptiles(j)) & (mat_real(:,i)<ptiles(j+1))),sum(N(1:i-1))+j) = 1;
+        end
+    end
+    
+elseif strcmp(how, 'means')
+    warning('OFF','stats:kmeans:EmptyCluster')
+    warning('OFF','stats:kmeans:FailedToConverge')
+    for i=1:m
+        [ids_tmp, c] = kmeans(mat_real(:,i), N(i), 'Start', 'uniform', 'EmptyAction', 'singleton');
+        [y,clust_id] = sort(c);
+        for j=1:length(clust_id)
+            mat_bin(ids_tmp==clust_id(j),sum(N(1:i-1))+j) = 1;
+        end
+        bounds{i} = y(1:end-1) + diff(y)/2;
+    end
+    
+else
+    error('How do you want to bin the data (width/height/means)?')
+end
